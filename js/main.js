0a5a5801c77272b3b640f4f6460890d83f2db136
Rimantas <belauzas@yahoo.com>
2020-11-25 17:22:24 +0200

    Print to DOM

--- js/main.js
+++ js/main.js
@@ -6,5 +6,12 @@ const ats = vidurkis(skaiciai);
 const ats2 = vidurkis(skaiciai2);
 
 // Is pateiktu 4 skaiciu, vidurkis gaunasi: 6.
-console.log(`Is pateiktu ${skaiciai.length} skaiciu, vidurkis gaunasi: ${ats}.`);
-console.log(`Is pateiktu ${skaiciai2.length} skaiciu, vidurkis gaunasi: ${ats2}.`);
+const atsString1 = `Is pateiktu ${skaiciai.length} skaiciu, vidurkis gaunasi: ${ats}.`;
+const atsString2 = `Is pateiktu ${skaiciai2.length} skaiciu, vidurkis gaunasi: ${ats2}.`;
+
+
+const rez1DOM = document.querySelector('#rez1');
+const rez2DOM = document.querySelector('#rez2');
+
+rez1DOM.innerText = atsString1;
+rez2DOM.innerText = atsString2;
